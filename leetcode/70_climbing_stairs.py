a89a4fea21a5e1ae1c176e4c9ba5b4428b628a39
ssh00n <seunghunshin1284@gmail.com>
2023-03-23 19:56:36 +0900

    Solved

---
+++ leetcode/70_climbing_stairs.py
@@ -0,0 +1,11 @@
+# DP basic
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {1 : 1,
+                2 : 2}
+
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+
+        return memo[n]
