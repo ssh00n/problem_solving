789b109e6ed602f775f494a521b5cdf34920b399
ssh00n <seunghunshin1284@gmail.com>
2024-01-12 11:07:09 +0900

    19941(Greedy) 76ms->48ms

--- baekjoon/19941.py
+++ baekjoon/19941.py
@@ -3,22 +3,15 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-table = input().rstrip()
-
-mask_array = [0 for _ in range(N)]
-count = 0
-for i in range(len(table)):
-    if table[i] == "P":
-        start, end = i - K, i + K  # 햄버거를 탐색할 구간
-        if start < 0:
-            start = 0
-        if end >= N:
-            end = N - 1
-
-        for j in range(start, end + 1):
-            if table[j] == "H" and not mask_array[j]:
-                mask_array[j] = 1
-                count += 1
+arr = list(input())
+cnt = 0
+for i in range(N):
+    if arr[i] != "P":
+        continue
+    for j in range(i - K, i + K + 1):
+        if 0 <= j < N:
+            if arr[j] == "H":
+                arr[j] = "-"
+                cnt += 1
                 break
-
-print(count)
+print(cnt)
